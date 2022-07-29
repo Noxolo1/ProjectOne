fe55903b3ae5578305b6124a4b04dcfeb287a794
Nate <nwilson@MacBook-Pro.local>
2022-07-28 17:31:11 -0700

    Project Euler 1

--- MagicalPython.py
+++ MagicalPython.py
@@ -1,4 +1,7 @@
-while(True):
-    print("hello world")
-    if(True):
-        break
+add = 0
+for i in range (0,1000):
+    if(i%3 == 0 or i%5 == 0):
+        add = i + add
+
+answer = add
+print(answer)
